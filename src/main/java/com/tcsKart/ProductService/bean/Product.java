0a32843380cde406d91ce0c4dcea5b0234cc2db7
Rahul Lodhi <rahullodhimts@gmail.com>
2025-06-26 20:13:01 +0530

    first commit

---
+++ src/main/java/com/tcsKart/ProductService/bean/Product.java
@@ -0,0 +1,7 @@
+package com.tcsKart.ProductService.bean;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Product {
+}
---
+++ src/main/java/com/tcsKart/TcsKartECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.tcsKart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcsKartECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcsKartECommerceApplication.class, args);
+	}
+
+}
